71a2450b3f0d2cef0bc76c1653203f162bf2cc00
Heitor Belem <heitorbelem11@gmail.com>
2020-05-14 13:13:45 -0300

    ⚡️ Resolve o problema de uma maneira ruim

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,61 @@
-import express from 'express';
+import axios from 'axios';
+import fs from 'fs';
+import { SHA1 } from 'crypto-js';
 
-const app = express();
+interface AnswerJson {
+  numero_casas: number;
+  token: string;
+  cifrado: string;
+  decifrado: string;
+  resumo_criptografico: string;
+}
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Ola Mundo' });
+const api = axios.create({
+  baseURL: 'https://api.codenation.dev/v1/challenge/dev-ps',
 });
 
-app.listen(3333, () => {
-  console.log('Server up and listening on port 3333');
-});
+const main = async (): Promise<void> => {
+  const { data } = await api.get(
+    '/generate-data?token=97a803786d1876893738c3357557a94384ba7490',
+  );
+
+  const jsonContent = JSON.stringify(data);
+
+  fs.writeFile('./answer.json', jsonContent, error => {
+    if (error) throw error;
+    console.log('Saved');
+  });
+
+  fs.readFile('./answer.json', 'utf-8', (error, jsonObj) => {
+    if (error) throw error;
+
+    const readJson = JSON.parse(jsonObj) as AnswerJson;
+
+    const maxUnicode = 'z'.charCodeAt(0);
+    const minUnicode = 'a'.charCodeAt(0);
+    const jumps = readJson.numero_casas % 26;
+
+    readJson.decifrado = readJson.cifrado
+      .split('')
+      .map(encodedChar => {
+        const charUnicode = encodedChar.charCodeAt(0);
+        if (encodedChar.match(/[a-z]/)) {
+          if (charUnicode - jumps < minUnicode) {
+            // it has '-1' beacause starts counting by the previous element
+            const relative_jumps = jumps - (charUnicode - minUnicode) - 1;
+            return String.fromCharCode(maxUnicode - relative_jumps);
+          }
+          return String.fromCharCode(charUnicode - jumps);
+        }
+        return encodedChar;
+      })
+      .join('');
+    readJson.resumo_criptografico = SHA1(readJson.decifrado).toString();
+    fs.writeFile('./answer.json', JSON.stringify(readJson), err => {
+      if (err) throw err;
+      console.log('File updated');
+    });
+  });
+};
+
+main();
